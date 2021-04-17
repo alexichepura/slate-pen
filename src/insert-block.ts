8ca4f56376d705cf0d72d2c2bdeef6e45bcf3587
Alexi Chepura <a.chepura@gmail.com>
2021-04-17 18:29:45 +0300

    up

--- src/insert-block.ts
+++ src/insert-block.ts
@@ -1,18 +1,17 @@
-import { Editor, Node, Range, Transforms } from "slate"
-import { TPartialNode } from "./plugin"
+import { BaseElement, Editor, Range, Text, Transforms } from "slate"
 
-export const insertBlock = (editor: Editor, slateElement: TPartialNode, range: Range) => {
+export const insertBlock = (editor: Editor, slateElement: BaseElement, range: Range) => {
   const [node] = Editor.node(editor, range)
-  if (node && node.text === "") {
+  if (node && Text.isText(node) && node.text === "") {
     const [parent] = Editor.parent(editor, range)
     Transforms.unsetNodes(editor, Object.keys(parent), { at: range })
     Transforms.setNodes(editor, slateElement, { at: range })
   } else {
-    Transforms.insertNodes(editor, slateElement as Node, { at: range })
+    Transforms.insertNodes(editor, slateElement, { at: range })
   }
 }
 
-export const setBlock = (editor: Editor, slateElement: TPartialNode, range: Range) => {
+export const setBlock = (editor: Editor, slateElement: BaseElement, range: Range) => {
   const [node] = Editor.node(editor, range)
   if (!Editor.isEditor(node)) {
     const [parent] = Editor.parent(editor, range)
--- src/wrap-inline-and-text.ts
+++ src/wrap-inline-and-text.ts
@@ -1,26 +1,23 @@
-import { Editor, Element, Node, Text } from "slate"
+import { BaseElement, Editor, Node, Text } from "slate"
 
 // https://github.com/ianstormtaylor/slate/issues/3457
 export const wrapInlineAndText = (
   editor: Editor,
   fragment: Node[],
-  defaultType: string
-): Element[] => {
-  let non_blocks: (Text | Element)[] = []
-  const blocks: Element[] = []
-
+  pusher: (children: (Text | BaseElement)[]) => BaseElement
+): BaseElement[] => {
+  let non_blocks: (Text | BaseElement)[] = []
+  const blocks: BaseElement[] = []
   const pushNewBlock = () => {
     if (non_blocks.length === 0) {
       return
     }
-    blocks.push({
-      type: defaultType,
-      children: non_blocks,
-    })
+    const item = pusher(non_blocks)
+    blocks.push(item)
     non_blocks = []
   }
 
-  fragment.forEach(f => {
+  fragment.forEach((f) => {
     if (Text.isText(f) || Editor.isInline(editor, f)) {
       non_blocks.push(f)
     } else {
